6fb5fb6597058f16e4ab6420f889abc6afcfe720
Brother_Gyu <brothergyu98@gmail.com>
2023-07-13 00:44:01 +0900

    [refact] register.py:Simplify code logic

--- Frontend/pages/register.py
+++ Frontend/pages/register.py
@@ -1,13 +1,9 @@
 import os
-import yaml
-import urllib.parse
-import requests
 import streamlit as st
 
-from PIL import Image
 from streamlit_extras.switch_page_button import switch_page
-
 from src.mongodb_cls import MongoDB_cls
+from src.thumbnail_decoder import thumbnail_decoder
 
 def register(username, password, check_list):
     mongodb = MongoDB_cls()
@@ -32,6 +28,7 @@ def register(username, password, check_list):
 
 
 def register_page():
+    mongodb = MongoDB_cls()
     with open(os.path.join(os.getcwd(), 'style.css')) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
@@ -49,37 +46,27 @@ def register_page():
         st.markdown('---')
         st.subheader('선호하는 음식을 선택하세요 [5개 이상]')
         st.markdown('<hr style="margin-top: 0.5rem; margin-bottom: 0.5rem;">', unsafe_allow_html=True)
-    
-        col1, col2, col3, col4 = st.columns(4)
-        with col1:
-            # ----------- 하드 코딩 ------------ #
-            response = requests.get("https://recipe1.ezmember.co.kr/cache/recipe/2019/05/25/39ac1e73e998e88da300d38663242f0a1_m.jpg", stream=True)
-            image = Image.open(response.raw)
-            st.image(image, use_column_width=True)
-            check_01 = st.checkbox('checkbox',key="check01")
-        with col2:
-            response = requests.get("https://recipe1.ezmember.co.kr/cache/recipe/2016/02/16/d74c6731fce3dc72c5579b24083185f11_m.jpg", stream=True)
-            image = Image.open(response.raw)
-            st.image(image, use_column_width=True)
-            check_02 = st.checkbox('checkbox',key="check02")
-        with col3:
-            response = requests.get("https://recipe1.ezmember.co.kr/cache/recipe/2023/05/18/44c74d970c984f419d0e01a736542c421_m.jpg", stream=True)
-            image = Image.open(response.raw)
-            st.image(image, use_column_width=True)
-            check_03 = st.checkbox('checkbox',key="check03")
-        with col4:
-            response = requests.get("https://recipe1.ezmember.co.kr/cache/recipe/2023/02/02/41942668ad91ccfb305c4cab9dee030f1_m.jpg", stream=True)
-            image = Image.open(response.raw)
-            st.image(image, use_column_width=True)
-            check_04 = st.checkbox('checkbox',key="check04")
-        
+
+
+        thumbnail_ids = [6912734, 6843136, 7002443, 6996297, 6885909]
+
+        cols = st.columns(5)
+        checkboxes = []
+        for i, thumbnail_id in enumerate(thumbnail_ids):
+            col = cols[i]
+            with col:
+                thumbnail = thumbnail_decoder(mongodb.load_thumbnail(thumbnail_id))
+                st.image(thumbnail, use_column_width=True)
+                checkbox = st.checkbox(mongodb.load_title(thumbnail_id), key=f"check_box{i}")
+                checkboxes.append(checkbox)
+
         register_button = st.form_submit_button("회원가입")
-    check_list = [check_01, check_02, check_03, check_04]
+
     if register_button:
-        print(check_list)
-        register_complete = register(new_username, new_password, check_list)
+        register_complete = register(new_username, new_password, checkboxes)
         if register_complete:
             switch_page("streamlit_app")
 
 if __name__ == "__main__":
+    st.set_page_config(layout="wide")
     register_page()
