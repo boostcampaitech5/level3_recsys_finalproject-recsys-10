d10a3b15eeb66e889624f50cb356a129d2ebe076
Brother_Gyu <brothergyu98@gmail.com>
2023-07-13 01:26:33 +0900

    [fix] streamlit_app.py: add logo

--- Frontend/streamlit_app.py
+++ Frontend/streamlit_app.py
@@ -9,9 +9,13 @@ from pages.login import login_page
 def home():
     with open(os.path.join(os.getcwd(), 'style.css')) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    
+    st.image(os.path.join(os.getcwd(), 'img/logo.png'), use_column_width=True)
+
     st.sidebar.title("오늘의 레시피")
     st.sidebar.write(f'환영합니다')
     st.sidebar.write(f'로그인/회원가입을 해주세요')
+
     login_result = login_page()
     try:
         if login_result[0] == True:
