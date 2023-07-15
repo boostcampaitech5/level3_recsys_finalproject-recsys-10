3afda1186e84af4611960bf40a85323fe3be74aa
Brother_Gyu <brothergyu98@gmail.com>
2023-07-15 21:39:10 +0900

    [refact] mongodb_cls.py - add : docstring

--- Frontend/src/mongodb_cls.py
+++ Frontend/src/mongodb_cls.py
@@ -4,6 +4,13 @@ from pymongo import MongoClient
 
 class MongoDB_cls:
     def __init__(self):
+        """
+        Initializes a MongoDB client with the provided connection configuration.
+        Args:
+            None
+        Returns:
+            None
+        """
         with open('_key/mongodb_key.yaml') as config_file:
             config = yaml.safe_load(config_file)
         connect_id = urllib.parse.quote_plus(config['id'])
@@ -11,27 +18,69 @@ class MongoDB_cls:
         connect_ip, connect_port = config['ip'], config['port']
         self.client = MongoClient(f'mongodb://{connect_id}:{str(connect_password)}@{connect_ip}:{connect_port}')
         
+
     def get_database(self, database_name: str):
+        """
+        Retrieves a MongoDB database instance based on the provided database name.
+        Args:
+            database_name (str): The name of the MongoDB database.
+        Returns:
+            pymongo.database.Database: The MongoDB database instance.
+        """
         return self.client[database_name]
     
+
     def get_collection(self, database_name: str, collection_name: str):
+        """
+        Retrieves a MongoDB collection instance based on the provided database and collection names.
+        Args:
+            database_name (str): The name of the MongoDB database.
+            collection_name (str): The name of the MongoDB collection.
+        Returns:
+            pymongo.collection.Collection: The MongoDB collection instance.
+        """
         database = self.get_database(database_name)
         return database[collection_name]
     
+
     def login(self, username: str, password: str) -> bool:
+        """
+        Authenticates a user by checking the provided username and password against the stored user credentials.
+        Args:
+            username (str): The username of the user.
+            password (str): The password of the user.
+        Returns:
+            bool: True if the authentication is successful, False otherwise.
+        """
         collection = self.get_collection('recipe_app_db', 'user_login_db')
         user = collection.find_one({"username": username})
         if user and user["password"] == password:
             return True
         return False
 
+
     def load_thumbnail(self, recipeid:int) -> str:
+        """
+        Retrieves the thumbnail link associated with the given recipe ID.
+        Args:
+            recipeid (int): The ID of the recipe.
+        Returns:
+            str: The URL of the thumbnail image.
+        """
         collection = self.get_collection('recipe_app_db', 'thumbnail_data')
         reciped_data = collection.find_one({"recipeid": recipeid})
         recipeid_thumbnail = reciped_data['thumb_link']
         return recipeid_thumbnail
     
+
     def load_title(self, recipeid:int) -> str:
+        """
+        Retrieves the title associated with the given recipe ID.
+        Args:
+            recipeid (int): The ID of the recipe.
+        Returns:
+            str: The title of the recipe.
+        """
         collection = self.get_collection('recipe_app_db', 'ingresync_recipe_data')
         reciped_data = collection.find_one({"recipeid": recipeid})
         recipeid_title = reciped_data['title']
