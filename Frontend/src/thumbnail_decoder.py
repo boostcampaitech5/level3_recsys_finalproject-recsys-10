989adfc1f2d6ee142b88e7d20eda46db9d8f20d7
Brother_Gyu <brothergyu98@gmail.com>
2023-07-15 21:40:23 +0900

    [refact] add : docstring, exception handling

--- Frontend/src/thumbnail_decoder.py
+++ Frontend/src/thumbnail_decoder.py
@@ -3,9 +3,20 @@ from io import BytesIO
 from PIL import Image
 
 def thumbnail_decoder(image_data):
-    image_bytes = base64.b64decode(image_data)
+    """
+    Decodes and converts the image data from base64 to an Image object.
+    Args:
+        image_data (str): The base64-encoded image data.
+    Returns:
+        PIL.Image.Image: The decoded image as a PIL Image object.
+    """
     try:
-        image = Image.open(BytesIO(image_bytes)).convert('RGB')
-    except:
-        image = Image.open(BytesIO(image_bytes)).convert('RGBA')
-    return image
+        image_bytes = base64.b64decode(image_data)
+        try:
+            image = Image.open(BytesIO(image_bytes)).convert('RGB')
+        except:
+            image = Image.open(BytesIO(image_bytes)).convert('RGBA')
+        return image
+    except Exception as e:
+        # Handle any exceptions that might occur during decoding
+        raise ValueError(f"Error decoding image data: {e}")
