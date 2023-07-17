ca6a1a45ccafa96a26137ac5d7c1cf6f2458b32e
Brother_Gyu <brothergyu98@gmail.com>
2023-07-17 11:01:44 +0900

    [feat] load_recipe_info 기능 추가

--- Frontend/pages/main.py
+++ Frontend/pages/main.py
@@ -23,12 +23,14 @@ def main():
     st.header(f'{st.session_state.key}님을 위한 레시피 추천')
     st.markdown("---")
 
+    # 내중에 모델 아웃풋 리스트 들어갈 곳
     thumbnail_ids = [6912734, 6843136, 7002443, 6996297, 6885909,
                      6915088, 6897374, 6933760, 6846168, 6881099]
 
     cols = st.columns(5)
     box_good_buttons = []
     box_move_buttons = []
+    result = [False]
 
     for i, thumbnail_id in enumerate(thumbnail_ids):
         col = cols[i%5]
@@ -39,7 +41,6 @@ def main():
             if len(title) > 19:
                 title = title[:19] + ".."  # 텍스트 길이 제한
             st.text(title)
-            # st.text(mongodb.load_title(thumbnail_id))
             box_col_01, box_col_02 = st.columns(2)
             with box_col_01:
                 box_good_button = st.button('좋아요', key=f"box_good_{thumbnail_id}", use_container_width=True)
@@ -50,7 +51,11 @@ def main():
         if i%5 == 4:
             st.markdown('---')
             cols = st.columns(5)
-
+    
+    result = [thumbnail_ids[i] for i in range(len(thumbnail_ids)) if box_move_buttons[i]]
+    if sum(box_move_buttons) == 1:
+        st.session_state.recipe_id = result[0]
+        switch_page("load_recipe_info")
 
 
 if __name__ == "__main__":
