7ca97eb460f6d478b062b8ee82d7aea4c3398f4a
SunwooHwang <sunwoo1357@g.skku.edu>
2023-07-20 06:49:08 +0000

    [feat] recipe_dataset.py: load data for similarity

---
+++ Model/similarity/recipe_dataset.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+import ast
+
+def get_recipe_dataset(path): # 데이터베이스 위치를 입력받으면 레시피 데이터프레임을 반환하는 함수
+    recipe_df = pd.read_csv(os.path.join(path,'recipe_data_final.csv'),encoding='utf-8')
+    recipe_df['ingredients'] = recipe_df['ingredients'].apply(lambda x:ast.literal_eval(x))
+    return recipe_df
