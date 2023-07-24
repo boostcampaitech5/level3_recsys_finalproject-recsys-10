a501735566b2a935a2cddfcbfa652d5ab674374f
alexandra9975 <sandra7033@gmail.com>
2023-07-24 15:54:32 +0900

    [Backend]added post routers

--- Backend/01-fastapi/app/routes/__init__.py
+++ Backend/01-fastapi/app/routes/__init__.py
@@ -40,3 +40,37 @@ async def retrieve_recipe(id: str) :
         return recipe
     
     return "No such recipe"
+
+@router.post("/login", response_model=Response)
+async def check_user(user_name, password):
+    user = check_id_password(user_name, password)
+    return {
+        "status_code": 200,
+        "response_type": "success",
+        "description": "User data retrieved successfully",
+        "data": user
+    }
+
+async def check_id_password(user_name: str, password:str):
+    if (user := mongodb.engine["user_login_db"].find_one({"username": id})) is not None:
+        user["_id"] = str(user["_id"])
+        if user["password"] != password:
+            return None
+        return user
+    return None
+
+@router.post("/thumbnail/{recipeid}", response_model=Response)
+async def post_thumbnail(recipeid):
+    thumbnail = retrieve_thumbnail(recipeid)
+    return {
+        "status_code": 200,
+        "response_type": "success",
+        "description": "Recipe data retrieved successfully",
+        "data": thumbnail
+    }
+
+async def retrieve_thumbnail(id: str) :
+    if (recipe := mongodb.engine["thumbnail_data"].find_one({"recipeid": int(id)})) is not None:
+        return recipe["thumbnail_link"]
+    
+    return "No such recipe"
