02c915ce9409b8937cbe665afb316e445e2c0400
SunwooHwang <sunwoo1357@g.skku.edu>
2023-07-24 12:22:19 +0000

    [fix] recipe_dataset.py : similarity get pickle

--- Model/similarity/recipe_dataset.py
+++ Model/similarity/recipe_dataset.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import os
 import ast
+import pickle
 
 def get_recipe_dataset(path): # 데이터베이스 위치를 입력받으면 레시피 데이터프레임을 반환하는 함수
-    recipe_df = pd.read_csv(os.path.join(path),encoding='utf-8')
-    recipe_df['ingredients'] = recipe_df['ingredients'].apply(lambda x:ast.literal_eval(x))
-    return recipe_df
+    try:
+        with open(os.path.join(path,'recipe_data.pkl'), 'rb') as f:
+            recipe = pickle.load(f)
+    except:
+        recipe = pd.read_csv(os.path.join(path,'recipe_data_final.csv'))
+        recipe['ingredients'] = recipe['ingredients'].apply(lambda x:ast.literal_eval(x))
+        with open(os.path.join(os.path.join(path),'recipe_data.pkl'), 'wb') as f:
+            pickle.dump(recipe, f)
+    return recipe
